eccc0a097ff678267aea3ef7f0346c8d6d209aef
karl-jerico <jerico.jeks@gmail.com>
2023-12-27 16:36:17 +0800

    add event detail feature

--- frontend/src/pages/EventDetails.js
+++ frontend/src/pages/EventDetails.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 const EventDetails = () => {
-  return (
-    <h1>EventDetails</h1>
-  )
+    const params = useParams()
+
+    return (
+      <>
+          <h1>Event Detail Page</h1>
+          <p>{params.eventId}</p>
+          <p><Link to=".." relative='path'>Back</Link></p>
+      </>
+    )
 }
 
 export default EventDetails
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const EVENTS = [
+    { id: 'Package 1', title: 'Event 1' },
+    { id: 'Package 2', title: 'Event 2' },
+    { id: 'Package 3', title: 'Event 3' },
+    { id: 'Package 4', title: 'Event 4' }
+]
 
 const Events = () => {
-  return (
-    <h1>Events</h1>
-  )
+    return (
+        <>
+            <h1>Event Page</h1>
+            <ul>
+                {EVENTS.map((evnt) => (
+                    <li key={evnt.id}>
+                        <Link to={evnt.id}>{evnt.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default Events
