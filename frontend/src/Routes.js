92986e04d61688ba9cfcc1804dcd78bb77924357
karl-jerico <jerico.jeks@gmail.com>
2023-12-27 16:57:35 +0800

    Add events navigation

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -5,6 +5,7 @@ import EventDetails from "./pages/EventDetails"
 import NewEvents from "./pages/NewEvents"
 import EditEvent from "./pages/EditEvent"
 import RootLayout from "./layouts/RootLayout"
+import EventsRoot from "./layouts/EventsRoot"
 
 const router = createBrowserRouter([
     {
@@ -12,11 +13,13 @@ const router = createBrowserRouter([
       element: <RootLayout />,
     //   errorElement: <ErrorPage />,
       children: [
-        { path: '', element: <Home /> },
-        { path: 'events', element: <Events /> },
-        { path: 'events/:eventId', element: <EventDetails /> },
-        { path: 'events/new', element: <NewEvents /> },
-        { path: 'events/:eventId/edit', element: <EditEvent /> },
+        { index: true, element: <Home /> },
+        { path: 'events', element: <EventsRoot />, children:[
+            { index: true, element: <Events /> },
+            { path: ':eventId', element: <EventDetails /> },
+            { path: 'new', element: <NewEvents /> },
+            { path: ':eventId/edit', element: <EditEvent /> },
+        ]},
       ]
     },
   ])
