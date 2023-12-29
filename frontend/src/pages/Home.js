6156081626192bf24d57f49b67807826150c2ff8
karl-jerico <jerico.jeks@gmail.com>
2023-12-29 16:59:20 +0800

    finalize the code

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import PageContent from "../components/PageContent"
 
 const Home = () => {
   return (
-    <h1>Home</h1>
-  )
+    <PageContent title="Welcome!">
+      <p>Browse all our Different Species!!</p>
+    </PageContent>
+  );
 }
 
 export default Home
